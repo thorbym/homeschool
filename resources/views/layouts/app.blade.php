872528c958f65064982a96ba592fece4e8991b73
thorbym <markthorby@gmail.com>
2020-05-20 07:12:54 +0100

    Swap menu items so categories go first

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,16 +55,16 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
                                     @if (Auth::user()->isAdmin())
                                         <a class="dropdown-item" href="{{ route('categories') }}">
                                             {{ __('Categories') }}
                                         </a>
                                     @endif
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
