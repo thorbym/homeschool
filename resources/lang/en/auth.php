d3c6d1e24421580e5a70d266bb37d24ad9d89200
thorbym <markthorby@gmail.com>
2020-05-27 19:28:41 +0100

    Change login message to be more generic

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'Incorrect login details.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
