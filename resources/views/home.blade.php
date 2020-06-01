70473648e009f29f06fa3d2dfe333716ed24b77d
thorbym <markthorby@gmail.com>
2020-06-01 16:41:56 +0100

    Add credit for icons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -93,7 +93,8 @@
                 <p style="color: white">
                   <small>
                     All rights reserved to TeachEm ©2020. You can click to read our <a href="{{ route('privacyPolicy') }}">privacy policy</a> and our <a href="{{ route('terms') }}">terms and conditions</a>.<br />
-                    Want to add your event to our app? Or talk to us about advertising? Contact us on teachem.online2020@gmail.com
+                    Want to add your event to our app? Or talk to us about advertising? Contact us on teachem.online2020@gmail.com<br />
+                    Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
                   </small>
                 </p>
                 <br />
